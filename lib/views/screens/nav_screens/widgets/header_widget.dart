5d108f33ad47b6c777fa62ebfaaf2374f453052c
unknown <saiful.bahri2004@gmail.com>
2025-11-05 13:51:26 +0700

    add header widget finish

--- lib/views/screens/nav_screens/widgets/header_widget.dart
+++ lib/views/screens/nav_screens/widgets/header_widget.dart
@@ -5,7 +5,7 @@ class HeaderWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.20,
       child: Stack(
@@ -14,6 +14,73 @@ class HeaderWidget extends StatelessWidget {
             'assets/icons/searchBanner.jpeg',
             width: MediaQuery.of(context).size.width,
             fit: BoxFit.cover,
+          ),
+          Positioned(
+            left: 48,
+            top: 68,
+            child: SizedBox(
+              width: 250,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter Text',
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    color: const Color.fromARGB(255, 32, 9, 180),
+                  ),
+                  contentPadding: EdgeInsets.symmetric(
+                    horizontal: 12,
+                    vertical: 16,
+                  ),
+                  prefixIcon: Image.asset('assets/icons/searc1.png'),
+                  suffixIcon: Image.asset('assets/icons/cam.png'),
+                  fillColor: Colors.grey.shade200,
+                  filled: true,
+                  focusColor: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 311,
+            top: 78,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {},
+                overlayColor: WidgetStatePropertyAll(
+                  const Color.fromARGB(255, 7, 84, 179),
+                ),
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icons/bell.png'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 354,
+            top: 78,
+            child: Material(
+              type: MaterialType.transparency,
+              child: InkWell(
+                onTap: () {},
+                child: Ink(
+                  width: 31,
+                  height: 31,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/icons/message.png'),
+                    ),
+                  ),
+                ),
+              ),
+            ),
           ),
         ],
       ),
